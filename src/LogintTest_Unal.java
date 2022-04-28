6be7e831f2c0fcb15100c9ae1ec8641582789c9f
unaltugrul <unaltugrulbulut@gmail.com>
2022-04-28 22:01:24 +0300

    AC1 is complated

--- src/LogintTest_Unal.java
+++ src/LogintTest_Unal.java
@@ -1,5 +1,9 @@
 public class LogintTest_Unal {
     public static void main(String[] args) {
         System.out.println("Login class is created");
+
+        System.out.println("AC1 testing is PASSED");
     }
+
+
 }
